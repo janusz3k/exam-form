a261a45762199502c968e7d1844a387c24717aab
Hubert Januszek <hubert.januszek.kielce@gmail.com>
2022-12-04 10:42:01 +0100

    Init exam commit

---
+++ src/connection.php
@@ -0,0 +1,6 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $haslo = "";
+    $db_name = "exam";
+?>
---
+++ src/index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <input type="text" name="login" placeholder="Login" /> <br>
+        <input type="password" name="pass" placeholder="Hasło" /> <br>
+        <input type="submit" value="Zaloguj się" />
+    </body>
+</html>
